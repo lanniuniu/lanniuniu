018c8c600954eb8d6003afb751f602d3d0e7f3dc
liupeng <1129638103@qq.com>
2017-02-22 18:26:19 +0800

    事件和表单脚本

---
+++ books/JavaScript高级程序设计/21Ajax与Comet/21-1.js
@@ -0,0 +1,43 @@
+/**
+ * Created by 懒牛牛 on 2017/2/22.
+ */
+
+/*
+XMLHttpRequest对象
+
+用法
+open() 要发送的请求的类型、请求的URL、表示是否异步发送请求的布尔值
+send() 发送请求
+onreadystatechage事件
+readyState
+status
+ */
+
+/*
+http头部信息
+
+ */
+
+/*
+get请求
+post请求
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ books/JavaScript高级程序设计/21Ajax与Comet/21-2.js
@@ -0,0 +1,35 @@
+/**
+ * Created by 懒牛牛 on 2017/2/22.
+ */
+
+/*
+XMLHttpRequest 2级
+ */
+
+/*
+FormData对象 （序列化）
+append()传入键值对
+ */
+
+/*
+超时设定
+timeout属性
+ontimeout事件
+ */
+
+/*
+overrideMimeType()方法  重写XHR响应的MIME类型
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
