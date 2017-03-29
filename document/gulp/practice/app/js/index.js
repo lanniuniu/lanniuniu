41c5422ae03a58f85cb5ee42383f0afa62be2b83
liupeng <1129638103@qq.com>
2017-03-29 09:53:07 +0800

    条纹背景和gulp demo页

--- document/gulp/practice/app/js/index.js
+++ document/gulp/practice/app/js/index.js
@@ -1,6 +1,15 @@
 /**
  * Created by 懒牛牛 on 2017/3/24.
  */
-let sum = (a,b)=>(a+b);
-sum(1,2);
+let currentTime = document.querySelector("#currentTime");
+currentTime.addEventListener('touchstart',(startObj)=>{
+    "use strict";
+    let moveStart;
+    moveStart = startObj.touches[0].clientX;
+    currentTime.addEventListener('touchend',(endObj)=>{
+        let move = endObj.changedTouches[0].clientX - moveStart;
+        console.log(move)
+    })
+},false);
+
 
