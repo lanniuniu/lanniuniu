87af86075c503c9a5b5bfa2f936bf18d5e21f506
liupeng <1129638103@qq.com>
2017-05-10 14:25:00 +0800

    xshell公约免密码登录

---
+++ qinQin/关于linux.js
@@ -0,0 +1,19 @@
+/**
+ * Created by 懒牛牛 on 2017/5/10.
+ */
+
+/*
+1.软件安装
+    分为在线安装和线下安装
+    在线安装：a.软件管理中心安装（ubuntu）需要图形化管理界面
+            b.命令行安装 1)更新apt-get update 2)查找你要安装的软件 apt-cache search 软件名
+                3)下载并安装及其依赖 apt-get install 软件名
+    线下安装：下载文件（tar、deb）到本地进行安装
+
+ */
+
+/*
+xshell公钥登录
+
+使用xshell在线传输工具（窗口>新建传输文件），下载到/root/.ssh/ ，更改文件名为authorized_keys
+ */
