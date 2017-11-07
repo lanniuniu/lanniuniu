e465607371f943151c43bc0e612a99c7607d38d9
liupeng <1129638103@qq.com>
2017-11-07 16:53:10 +0800

    fis3命令行工具

---
+++ document/fis3/7 接口文档/7-1 命令行.js
@@ -0,0 +1,34 @@
+/**
+ * Created by 懒牛牛 on 2017/11/7.
+ */
+
+/*
+fis3 -h 查询提供了哪些内置命令
+
+ */
+
+/*
+fis3 release 编译发布一个 FIS3 项目
+
+ */
+
+/*
+fis3 install 用来从组件平台中下载组件到当前项目中，并自动下载其依赖。默认组件下载来源于 fis-components 机构。
+
+ */
+
+/*
+fis3 init
+fis3 脚手架工具，用来快速初始化项目。
+ */
+
+/*
+fis3 server
+fis3 内置了一个小型 web server, 可以通过 fis3 server start 快速开启。如果一切正常，开启后它将自动弹出浏览器打开 http://127.0.0.1:8080/。
+ */
+
+/*
+fis3 inspect
+用来查看文件 match 结果。
+ */
+
