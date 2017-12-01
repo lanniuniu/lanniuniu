3b2f8ae9f101e2c2d39a45083ca4dc6bfab7d2d7
liupeng <1129638103@qq.com>
2017-12-01 14:11:11 +0800

    eggjs

---
+++ document/egg/33代码风格指南/33-1.js
@@ -0,0 +1,22 @@
+/**
+ * Created by 懒牛牛 on 2017/12/1.
+ */
+
+/*
+建议开发者使用 egg-init --type=simple showcase 来生成并观察推荐的项目结构和配置。
+
+ */
+
+/*
+Classify
+
+ */
+
+/*
+Private property && Lazy Initialization
+
+私有属性用 Symbol 来挂载。
+Symbol 的描述遵循 jsdoc 的规则，描述映射后的类名+属性名。
+延迟初始化。
+ */
+
