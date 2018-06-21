15a44503a346d016638e5c5cd0873a63be7ccc05
lanniuniu <1129638103@qq.com>
2018-06-21 16:08:48 +0800

    angular动画

---
+++ document/angular/2 组件与模板/2-8 动画.ts
@@ -0,0 +1,21 @@
+/*
+import { BrowserAnimationsModule （动画模块） } from '@angular/platform-browser/animations';
+
+在跟模块引入
+ */
+
+/*
+状态与转场
+
+可动的(Animatable)属性与单位
+
+自动属性值计算
+
+动画时间线
+
+基于关键帧(Keyframes)的多阶段动画
+
+并行动画组
+
+动画回调
+ */
