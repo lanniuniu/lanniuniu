3f9923979d634ac520388faece1ea0b67d2a7d33
lanniuniu <1129638103@qq.com>
2018-08-09 17:50:36 +0800

    react

--- document/react/7 表单/form.js
+++ document/react/7 表单/form.js
@@ -43,3 +43,30 @@ input 和 textarea 同理
 和 <select> 都十分类似 - 他们都通过传入一个value属性来实现对组件的控制。
  */
 
+/*
+文件上传
+由于value值是只读的，所以它是非受控组件
+ */
+
+/*
+多个输入的解决方法
+当你有处理多个受控的input元素时，
+你可以通过给每个元素添加一个name属性，来让处理函数根据 event.target.name的值来选择做什么。
+ */
+function handleInputChange(event) {
+  const target = event.target;
+  const value = target.type === 'checkbox' ? target.checked : target.value;
+  const name = target.name;
+
+  // 使用ES6当中的计算属性名语法来更新与给定输入名称相对应的状态键
+  this.setState({
+    [name]: value
+  });
+}
+
+/*
+受控组件的替代方法 —— 非受控组件
+使用受控组件很繁琐，因为每一个变化都可能加上事情处理，所以也可以使用代替技术 非受控组件
+ */
+
+
