2e1681640ab83ce992dd64e2851ed09856110d6d
LiuPeng <1129638103@qq.com>
2020-04-09 20:44:19 +0800

    重学前端：css元信息类标签、选择器

--- online-course/relearn front end/2 html和css.js
+++ online-course/relearn front end/2 html和css.js
@@ -39,5 +39,20 @@ max() min() clamp() 给定范围 超出范围取范围值
 toggle() 规则选中多于一个元素时生效 来回切换效果
 
 3、元信息类标签
+base: 废弃标签，给所有的url加相对地址 用js代替它
+meta: 一组键值对标签，表示通用的信息元 name content
+    定义编码 charset的meta
+    定义http请求 http-equiv
+    定义移动端页面 viewport  可以设置 width height device-width device-height initial-scale minimum-scale maximum-scale user-scalable
 
+meta标签还有很多各种各样的属性，可以提供丰富的功能
+
+4、选择器
+命名空间的类型选择器?
+属性选择器 四种 [att] [attr=val] [attr~=val1 val2 val3]多种值匹配 [attr|=val]开头匹配
+伪类选择器：树结构关系、链接与行为、逻辑（:not）
+复杂选择器连接：空格（后代） >(子) ~（后继） +（直接后继）||（列选择）
+伪元素：::after ::before ::first-line ::first-letter
+
+5、标签
  */
