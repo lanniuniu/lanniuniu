6000ff566d4cda11d7401267848803151bd2999f
LiuPeng <1129638103@qq.com>
2020-04-15 14:01:56 +0800

    flex: 总结

---
+++ something to improve/关于flex.js
@@ -0,0 +1,11 @@
+/*
+1、主轴、交叉轴（位置不固定，取决于flex-direction）
+2、flex-wrap: 是否换行nowrap wrap wrap-reverse
+3、flex-flow = flex-direction + flex-wrap
+4、flex = flex-grow + flex-shirk + flex-basis
+5、justify-content 主轴对齐
+6、align-items 交叉轴对齐 默认值stretch 会占满整个交叉轴的长度
+7、align-content 当flex-wrap为wrap时 交叉轴对齐设置多行对齐
+8、align-self 对某一个元素进行单独设置 交叉轴对齐
+9、order：排队顺序
+ */
---
+++ something to improve/关于ngix.js
@@ -0,0 +1,3 @@
+/*
+https://juejin.im/post/5bd7a6046fb9a05d2c43f8c7
+ */
