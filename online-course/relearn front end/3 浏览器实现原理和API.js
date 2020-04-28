06c0c73a80fb7797fbe95557d393144b05f5e4a1
LiuPeng <1129638103@qq.com>
2020-04-28 20:20:42 +0800

    重学前端：http request

--- online-course/relearn front end/3 浏览器实现原理和API.js
+++ online-course/relearn front end/3 浏览器实现原理和API.js
@@ -1,4 +1,5 @@
 /*
 1、尽可能流式进行上一步输出 DNS、http、DOM、css计算、渲染合成、绘制到页面
-2、
+2、http put delete head（只返回请求头） connect（用于https websocket）
+301永久转移 302暂时转移 304 前提是本地有缓存 通过request告诉服务端 服务端通过时间或tag 发现没得更新 就返回304
  */
