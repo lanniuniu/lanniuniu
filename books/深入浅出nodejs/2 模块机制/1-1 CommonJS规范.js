4094a39f11df8909448d44b053506661ae28574e
LiuPeng <1129638103@qq.com>
2020-05-13 23:52:32 +0800

    深入浅出nodejs：Node的应用场景和CommonJS规范

---
+++ books/深入浅出nodejs/2 模块机制/1-1 CommonJS规范.js
@@ -0,0 +1,9 @@
+/*
+1、Node、浏览器、W3C组织、CommonJS组织、ECMAScript组织 共同构成了一个繁荣的生态系统
+2、CommonJS规范涵盖了模块、二进制、Buffer、字符集编码、I/O流、进程环境、文件系统、套接字、单元测试、Web服务器网关接口、包管理
+3、CommonJS出发点：包管理、模块、各种标准接口
+4、模块规范：模块引用require、模块定义export、模块标识fs
+
+在node中 module对象代表模块自身 而export是module的属性 所以 module.export = export
+ */
+
