6549595b12e9a3831653d310f4d69d484b2a020b
LiuPeng <1129638103@qq.com>
2020-05-14 23:59:32 +0800

    深入浅出nodejs: 模块实现

--- books/深入浅出nodejs/2 模块机制/1-2 Node的模块实现.js
+++ books/深入浅出nodejs/2 模块机制/1-2 Node的模块实现.js
@@ -4,4 +4,9 @@
     部分核心模块会直接加载进内存 加载速度最快
 3、Node模块加载过程：
     优先从缓存加载
+    路径分析：模块标识符分析（速度快慢区别） 文件定位：文件扩展名分析 js node json 后两个加扩展名速度更快
+    模块编译：
+        js __filename __dirname
+        node（c/c++） process.dlopen()
+        json(fs)
  */
