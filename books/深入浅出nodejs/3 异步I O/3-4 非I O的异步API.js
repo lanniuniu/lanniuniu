ca8dd24058a544b5ad414f6a9c5169e87a54216a
LiuPeng <1129638103@qq.com>
2020-05-27 17:14:56 +0800

    深入浅出nodejs: 非I/O的API 和 高性能web服务器

--- books/深入浅出nodejs/3 异步I O/3-4 非I O的异步API.js
+++ books/深入浅出nodejs/3 异步I O/3-4 非I O的异步API.js
@@ -1,5 +1,11 @@
 /*
 1、定时器 setTimeout setInterval()
 延时不精确  因为不确定事件循环的当前循环的占用情况
-2、
+
+2、process.nextTick() 更为高效的立即异步执行
+
+3、setImmediate() 和 process.nextTick() 很相似
+但是process.nextTick() 属于idle观察者 setImmediate()属于check观察者
+而在每一轮事件循环中  idle观察者 先于 I/O观察者 先于check观察者
+
  */
