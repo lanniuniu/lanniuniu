7ffa56ff4e5078cc1e4f564636f3fd8d17a8e047
LiuPeng <1129638103@qq.com>
2020-07-15 18:10:09 +0800

    深入浅出nodejs: http

---
+++ books/深入浅出nodejs/7 网络编程/7-3 构建http服务.js
@@ -0,0 +1,14 @@
+/*
+1、TCP 和 UDP都属于网络传输层的协议，要构建高性能的网络应用，就从她们着手。但是对于普通的网络应用，应用层协议http 绰绰有余
+
+2、通过延迟 res.end() 可以实现服务端和客户端的长连接  但结束务必关闭连接
+
+3、HTTP服务的事件
+connection request close checkContinue connect upgrade clientError
+
+4、http客户端
+http.request()
+
+5、http代理
+http.globalAgent
+ */
