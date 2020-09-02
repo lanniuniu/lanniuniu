5b7147f95e431bfe1646b00fc1da8b96b1c6f15c
spiritsliu <spiritsliu@tencent.com>
2020-09-02 17:53:41 +0800

    深入浅出nodejs: 内容响应

---
+++ books/深入浅出nodejs/8 构建Web应用/8-5 页面渲染.js
@@ -0,0 +1,17 @@
+/*
+内容响应 和 页面渲染
+1、内容响应
+响应报头中content-* 十分重要  客户端会根据这些内容 进行内容的解析
+MIME：Multipurpose Internet Mail Extensions
+content-type 里面含有MIME的值  浏览器会根据这个字段来决定采用不同的渲染方式
+
+附件下载
+Content-Disposition 根据这个值来判断是否下载 当为内容查看 inline 当为附件下载 attachment
+
+JSON
+
+响应跳转 header location url 响应码320
+
+2、视图渲染
+
+ */
