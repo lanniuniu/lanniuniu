7ce044b967646940f6f05afd08f0549d8a19de0a
spiritsliu <spiritsliu@tencent.com>
2020-09-03 17:01:39 +0800

    深入浅出nodejs: 模板函数和MVC下的页面渲染

--- books/深入浅出nodejs/8 构建Web应用/8-5 页面渲染.js
+++ books/深入浅出nodejs/8 构建Web应用/8-5 页面渲染.js
@@ -13,5 +13,46 @@ JSON
 响应跳转 header location url 响应码320
 
 2、视图渲染
+模板：ASP PHP JSP
+模板语言 包含模板语言的模板文件 拥有动态数据的数据对象 模板引擎
 
+模板引擎: 语法分解、处理表达式、生成待执行的语句、与数据一起执行，生成最终的字符串
  */
+
+/**
+ * 模板函数
+ * @param str
+ * @param data
+ * @returns {*}
+ */
+const render = (str, data) => {
+    // JSP ASP模板语言
+    const JSPREG = /<%=([\s\S]+?)%>/g
+    let tpl = str.replace(JSPREG, (match, code) => {
+        console.log(match)
+        return `' + obj.${code}+ '`
+    })
+    console.log(tpl)
+    tpl = `var tpl = '${tpl}'\nreturn tpl;`
+    console.log(tpl)
+    const complied = new Function('obj', tpl) // 变量名为函数名 最后一个参数为函数内容 其他为参数 构造函数所有入参都为字符串
+    return complied(data)
+}
+const tpl = `love <%=username%>.`
+console.log(render(tpl, {username: 'qinqin'}))
+
+/*
+优秀的模板引擎远不止这些，可以改善
+with的应用
+
+模板安全 xss漏洞
+模板逻辑
+集成文件系统 综合缓存和文件读取
+子模板
+布局视图
+模板性能 优化执行表达式
+
+以上均为 MVC模型下的页面渲染
+现在都用MVVM
+ */
+
