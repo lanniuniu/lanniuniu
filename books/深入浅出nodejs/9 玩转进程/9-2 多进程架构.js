ea03feed9ecfd59100a63f7f9a216096952f2e3d
spiritsliu <spiritsliu@tencent.com>
2020-09-07 16:51:31 +0800

    深入浅出nodejs: 子进程和利用子进程、句柄等搭建集群

--- books/深入浅出nodejs/9 玩转进程/9-2 多进程架构.js
+++ books/深入浅出nodejs/9 玩转进程/9-2 多进程架构.js
@@ -22,4 +22,38 @@ for (let i = 0; i < cpus.length; i++) {
 分布式架构 用于并行处理业务 具有良好的可伸缩性和稳定性
 
 但是fork()进程是昂贵的 10MB的内存和30ms的启动时间
+
+创建子进程
+spawn() exec() execFile() fork() 各不相同
+后面三种是spawn的延伸应用
+ */
+const cp = require('child_process')
+cp.spawn('node', ['worker.js'])
+cp.exec('node worker.js', (err, stdpit, stderr) => {
+
+})
+cp.execFile('worker.js', (err, stdpit, stderr) => {
+
+})
+cp.fork('./worker.js')
+
+/*
+前端使用webworker 通过onmessage() postMessage() 进行通信
+
+子进程对象相似地 由send() 和 message事件共同实现通信
+
+IPC即进程间通信 要求两个进程均为node
+
+句柄
+种类如下
+net.Socket tcp套接字
+net.Server tcp服务器
+net.Native c++层面的tcp套接字或ipc管道
+dgram.Socket udp套接字
+dgram.Native c++层面的udp套接字
+ */
+
+/*
+创建子进程、进程间通信的IPC通道实现、句柄在进程间的发送和还原、端口共用
+这些技术 可以在单机上搭建Node集群 所以可以在多核CPU上 可以充分利用资源
  */
---
+++ books/深入浅出nodejs/9 玩转进程/9-3 集群稳定之路.js
@@ -0,0 +1,15 @@
+/*
+进程时间
+error
+exit
+close
+disconnect
+
+自动重启  自杀信号 限量重启
+
+负载均衡 Round-Robin（轮叫调度）
+
+状态共享
+第三方数据共享 数据库、磁盘文件、缓存服务（redis）
+主动通知
+ */
