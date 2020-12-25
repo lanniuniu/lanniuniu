21105936422ec67a11f140c2772d7d77f3dee533
spiritsliu <spiritsliu@tencent.com>
2020-12-25 11:30:20 +0800

    electron: 自动更新

--- document/electron/1 教程/1-21 自动更新功能.js
+++ document/electron/1 教程/1-21 自动更新功能.js
@@ -8,4 +8,6 @@
     电子发布服务器 ： 一个功能齐全的自主发布的电子应用服务器，与自动更新兼容
     squirrel-updates-server ： 用于Squirrel.Mac和Squirrel.Windows的简单node.js服务器，它使用GitHub版本
     squirrel-release-server ： 一个用于Squirrel.Windows的简单PHP应用程序，用于从文件夹读取更新。 支持增量更新。
+
+autoUpdater模块提供了统一的api  但在各平台上会有差异
  */
