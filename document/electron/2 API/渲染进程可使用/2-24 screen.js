049d4bcedf4905e864a1f89cb90db247b5bce514
spiritsliu <spiritsliu@tencent.com>
2021-05-06 17:21:17 +0800

    electron: screen shell

---
+++ document/electron/2 API/渲染进程可使用/2-24 screen.js
@@ -0,0 +1,11 @@
+/*
+检索屏幕的 size，显示，鼠标位置等 信息
+使用前提：ready
+
+window.screen 是BOM 内置对象  不能用这个进行覆盖
+
+
+Display 对象
+Display 对象表示了物力方式连接系统.
+一个伪造的 Display 或许存在于一个无头系统中，或者一个 Display 相当于一个远程的、虚拟的 display.
+ */
